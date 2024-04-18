4846e0e75aa7887e988adc4671dd2e7cbed36c8c
ANUPAM KUMAR SINGH <anupamkumar46511@gmail.com>
2024-04-18 16:04:37 +0530

    dashboard created

--- api/register.php
+++ api/register.php
@@ -9,31 +9,32 @@ $image = $_FILES['photo']['name'];
 $tmp_name = $_FILES['photo']['tmp_name'];
 $role = $_POST['role'];
 
-if ($password == $Cpassword) {
-    move_uploaded_file($tmp_name,'../uploads/$image');
-    $insert = mysqli_query($connect, "INSERT into user(name,mobile,address,password,photo,role,status,votes) VALUES ('$name','$mobile','$address','$password','$image','$role',0,0)");
-    if ($insert) {
-        echo '
+    if ($password == $Cpassword) {
+        move_uploaded_file($tmp_name, '../uploads/$image');
+        $insert = mysqli_query($connect, "INSERT into user(name,mobile,address,password,photo,role,status,votes) VALUES ('$name','$mobile','$address','$password','$image','$role',0,0)");
+        if ($insert) {
+            echo '
         <script>
         alert("registration succefull !");
         window.location = "../";
         </script>
         ';
-    }
-    else {
-        echo '
+        } else {
+            echo '
         <script>
         alert("some error occured !");
         window.location = "../routes/register.html";
         </script>
         ';
-    }
-} else {
-    echo '
+        }
+    } else {
+        echo '
     <script>
     alert("Password and Confirm password does not match!");
     window.location = "../routes/register.html";
     </script>
     ';
-}
+    }
+
+
 ?>
